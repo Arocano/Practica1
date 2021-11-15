803954b512f34f5c452ffc3acd82b617c303f450
Alejandro Rocano <alejandrorocano1@gmail.com>
2021-11-15 18:30:18 -0500

    Añadir Empleado

---
+++ Empleado.java
@@ -0,0 +1,10 @@
+public class Empleado(){
+
+          private String cedula;
+          private String nombre;
+
+          public getCedula String(){
+           cedula=this.cedula;
+           return cedula;}
+
+}
